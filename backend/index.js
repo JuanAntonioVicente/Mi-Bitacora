df37412532613c057dc72887593ec8759f9597e7
Juan Antonio Vicente <juanantoniovicenteruiz@hotmail.com>
2026-09-17 20:48:46 +0200

    Add CORS config, app title and favicon

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import { pool } from "./db.js";
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
